27edd9bf5011654ba7c2044ee089f9d519b7a29c
Anna Silinskaya <annsilin@mail.ru>
2023-08-01 22:55:08 +0400

    fix: make bars in burger icon clickable

--- library/assets/js/burger.js
+++ library/assets/js/burger.js
@@ -1,16 +1,17 @@
 let burgerButton = document.getElementById("burger-button");
+let burgerButtonBars = document.getElementsByClassName("burger-button__bar")
 let navigation = document.getElementById("navigation");
 let navigationMenu = document.getElementById("navigation-menu");
 let navigationIcons = document.getElementById("navigation-icons");
 let navigationLinks = document.getElementById("navigation__links");
 
-burgerButton.addEventListener("click", () => {
+[burgerButton, burgerButtonBars].forEach(e => e.addEventListener("click", () => {
   burgerButton.classList.toggle("onclick");
   navigation.classList.toggle("onclick");
   navigationMenu.classList.toggle("onclick");
   navigationIcons.classList.toggle("onclick");
   navigationLinks.classList.toggle("onclick");
-})
+}))
 
 document.onclick = (e) => {
   if (![navigation, navigationMenu, navigationIcons, navigationLinks, burgerButton].includes(e.target)) {
