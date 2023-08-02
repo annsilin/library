964ce004649cb9cad720d00a2ddd40df0e4fe4ab
Anna Silinskaya <annsilin@mail.ru>
2023-08-03 00:05:56 +0400

    refactor: restructure navigation \n feat: add burger menu animation

--- library/assets/js/burger.js
+++ library/assets/js/burger.js
@@ -1,24 +1,18 @@
 let burgerButton = document.getElementById("burger-button");
 let navigation = document.getElementById("navigation");
-let navigationMenu = document.getElementById("navigation-menu");
-let navigationIcons = document.getElementById("navigation-icons");
 let navigationLinks = document.getElementById("navigation__links");
 
 burgerButton.addEventListener("click", () => {
   event.stopPropagation();
   burgerButton.classList.toggle("onclick");
   navigation.classList.toggle("onclick");
-  navigationMenu.classList.toggle("onclick");
-  navigationIcons.classList.toggle("onclick");
   navigationLinks.classList.toggle("onclick");
 })
 
 document.onclick = (e) => {
-  if (![navigation, navigationMenu, navigationIcons, navigationLinks, burgerButton].includes(e.target)) {
+  if (![navigation, navigationLinks, burgerButton].includes(e.target)) {
     burgerButton.classList.remove("onclick");
     navigation.classList.remove("onclick");
-    navigationMenu.classList.remove("onclick");
-    navigationIcons.classList.remove("onclick");
     navigationLinks.classList.remove("onclick");
   }
 }
