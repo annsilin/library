6bbe700a8a9850a70a166efb70f0159b3d99bf88
Anna Silinskaya <annsilin@mail.ru>
2023-09-05 23:09:56 +0400

    feat: change profile modal according to logged in user

---
+++ library/assets/js/profile-modal.js
@@ -0,0 +1,25 @@
+const updateProfileModal = (user) => {
+  document.querySelector(".modal-profile__user-pic").innerText = Array.from(user.firstName)[0].toUpperCase() +
+    Array.from(user.lastName)[0].toUpperCase();
+  document.querySelector(".modal-profile__user-name-first").innerText = user.firstName;
+  document.querySelector(".modal-profile__user-name-last").innerText = user.lastName;
+  document.querySelector(".modal-profile__info-card-visits").innerText = user.visits;
+  document.querySelector(".modal-profile__info-card-books").innerText = user.books.length;
+  document.querySelector(".modal-profile__info-library-card-number").innerText = user.cardNumber;
+
+  let booksUl = document.querySelector(".modal-profile__info-books-list");
+  for (let bookID of user.books) {
+    let foundBook = books.find(book => book.id === bookID);
+    let bookLi = document.createElement('li');
+    bookLi.className = 'modal-profile__info-books-list-item';
+    bookLi.innerHTML = `${foundBook.title}, ${foundBook.author}`;
+    booksUl.append(bookLi);
+  }
+}
+
+addEventListener("DOMContentLoaded", () => {
+  if (currentUser) {
+    updateProfileModal(currentUser);
+  }
+});
+
