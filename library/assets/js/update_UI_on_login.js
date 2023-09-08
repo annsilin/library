82c12eebaa2a14bde96297c5ef4a1655ba393bdd
Anna Silinskaya <annsilin@mail.ru>
2023-09-08 19:02:27 +0400

    feat: add 'title' attribute to profile icon

--- library/assets/js/update_UI_on_login.js
+++ library/assets/js/update_UI_on_login.js
@@ -2,7 +2,9 @@ const updateUI = () => {
   // Change profile button icon to first name and last name initials in header
   let btnProfile = document.querySelector(".btn-profile");
   btnProfile.innerHTML = `${Array.from(currentUser.firstName)[0].toUpperCase() + Array.from(currentUser.lastName)[0].toUpperCase()}`;
-  btnProfile.style.backgroundColor = '#FFFFFF'
+  btnProfile.style.backgroundColor = '#FFFFFF';
+  let fullName = currentUser.firstName + " " + currentUser.lastName;
+  btnProfile.setAttribute('title', fullName);
 
   // Replace sign up and log in buttons with profile button in 'Get Card' section
   document.querySelector(".get-card-buttons-wrapper").style.display = 'none';
